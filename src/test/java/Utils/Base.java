f2f316b3c9b63e39c33540904d1a5b2988087045
Fikret <rehimlifikret2@gmail.com>
2024-07-31 01:34:09 +0400

    fix: solve TimeoutException problem!

--- src/test/java/Utils/Base.java
+++ src/test/java/Utils/Base.java
@@ -21,7 +21,8 @@ public class Base {
             cap.setCapability("appium:automationName", "UiAutomator2");
             cap.setCapability("appium:app", "C:\\Users\\Asus\\Downloads\\misli_android.apk");
             this.driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), cap);
-            this.wait = new WebDriverWait(driver, Duration.of(3, ChronoUnit.SECONDS));
+            this.wait = new WebDriverWait(driver, Duration.of(1, ChronoUnit.MINUTES));
+//            this.driver.manage().timeouts().implicitlyWait(Duration.of(1, ChronoUnit.MINUTES));
 
         } catch (Exception exception) {
             throw new RuntimeException(exception);
--- src/test/java/misli/loginElements.java
+++ src/test/java/misli/loginElements.java
@@ -5,18 +5,22 @@ import org.openqa.selenium.WebElement;
 
 
 public class loginElements {
-//    @AndroidFindBy (xpath = "//android.widget.TextView[@resource-id=\"com.misli.androidNew:id/text_view_update_not_now\"]")
-//        public WebElement tapnotnow;
+
     @AndroidFindBy (id = "com.misli.androidNew:id/text_view_update_not_now")
     public WebElement tapnotnow;
+
     @AndroidFindBy (id ="com.misli.androidNew:id/button_accept_all")
     public WebElement acceptall;
+
     @AndroidFindBy(id  = "com.misli.androidNew:id/button_login")
     public WebElement loginbtn;
+
     @AndroidFindBy(id = "com.misli.androidNew:id/text_input_username")
     public WebElement userNamefield;
+
     @AndroidFindBy(id = "com.misli.androidNew:id/text_input_password")
     public WebElement passwordfield;
+
     @AndroidFindBy(id = "com.misli.androidNew:id/login_sign_in_btn")
     public WebElement enterbtn;
 
